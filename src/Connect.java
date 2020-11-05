06212544fd694834f612fdaf29482b6253fea4e0
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2020-11-05 16:19:46 +0530

    Added:src

---
+++ src/Connect.java
@@ -0,0 +1,29 @@
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author
+ */
+public class Connect {
+    Connection con=null;
+   
+        public static Connection ConnectDB(){
+        try{
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagmentsystem?verifyServerCertificate=false&useSSL=true","root","saket2000");
+           return con;
+        }catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }      
+    }
+}
